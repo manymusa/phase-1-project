0d9d62f485ce956abbbfb9547b66814f0d0dd92d
manymusa <adanalin113@gmail.com>
2022-09-03 20:18:15 -0400

    added third event listener

--- index.js
+++ index.js
@@ -1,38 +1,46 @@
 const submitPassword = document.querySelector('form');
-const textField = document.querySelector('.text'); 
+const textField = document.querySelector('.text');
 const commonPasswordH2 = document.querySelector('#common-result');
 const compromisedPasswordH2 = document.querySelector('#compromised-result');
-const mostCommonPasswords = ['12345','123456','123456789','test1','password','12345678','zinch','g_czechout','asdf','qwerty','1234567890','1234567',,'Aa123456.','iloveyou','1234','abc123','111111','123123','dubsmash','test','princess','qwertyuiop','sunshine','BvtTest123','11111','ashley','00000','000000','password1','monkey','livetest','55555','soccer','charlie','asdfghjkl','654321','family','michael','123321','football','baseball','q1w2e3r4t5y6','nicole','jessica','purple','shadow','hannah','chocolate','michelle','daniel','maggie','qwerty123','hello','112233','jordan','tigger','666666','987654321','superman','12345678910','summer','1q2w3e4r5t','fitness','bailey','zxcvbnm','fuckyou','121212','buster','butterfly','dragon','jennifer','amanda','justin','cookie','basketball','shopping','pepper','joshua','hunter','ginger','matthew','abcd1234','taylor','samantha','whatever','andrew','1qaz2wsx3edc','thomas','jasmine','animoto','madison','0987654321','54321','flower','Password','maria','babygirl','lovely','sophie','Chegg123'];
+const mostCommonPasswords = ['12345', '123456', '123456789', 'test1', 'password', '12345678', 'zinch', 'g_czechout', 'asdf', 'qwerty', '1234567890', '1234567', , 'Aa123456.', 'iloveyou', '1234', 'abc123', '111111', '123123', 'dubsmash', 'test', 'princess', 'qwertyuiop', 'sunshine', 'BvtTest123', '11111', 'ashley', '00000', '000000', 'password1', 'monkey', 'livetest', '55555', 'soccer', 'charlie', 'asdfghjkl', '654321', 'family', 'michael', '123321', 'football', 'baseball', 'q1w2e3r4t5y6', 'nicole', 'jessica', 'purple', 'shadow', 'hannah', 'chocolate', 'michelle', 'daniel', 'maggie', 'qwerty123', 'hello', '112233', 'jordan', 'tigger', '666666', '987654321', 'superman', '12345678910', 'summer', '1q2w3e4r5t', 'fitness', 'bailey', 'zxcvbnm', 'fuckyou', '121212', 'buster', 'butterfly', 'dragon', 'jennifer', 'amanda', 'justin', 'cookie', 'basketball', 'shopping', 'pepper', 'joshua', 'hunter', 'ginger', 'matthew', 'abcd1234', 'taylor', 'samantha', 'whatever', 'andrew', '1qaz2wsx3edc', 'thomas', 'jasmine', 'animoto', 'madison', '0987654321', '54321', 'flower', 'Password', 'maria', 'babygirl', 'lovely', 'sophie', 'Chegg123'];
 const regexTest = {
     'containsLowerCase': /[a-z]/,
-    'containsUpperCase' : /[A-Z]/,
-    'containsNumber' : /[0-9]/,
-    'containsSpecialCharacter' : /\W/,
+    'containsUpperCase': /[A-Z]/,
+    'containsNumber': /[0-9]/,
+    'containsSpecialCharacter': /\W/,
     'passwordLength': /^.{12,}/
 };
 
-submitPassword.addEventListener('submit',(e) =>{
+submitPassword.addEventListener('submit', (e) => {
     e.preventDefault();
-    textContent =  textField.value;
+    textContent = textField.value;
     isPasswordCompromised(textContent);
-    passwordComparison(mostCommonPasswords,textContent,commonPasswordH2);
+    passwordComparison(mostCommonPasswords, textContent, commonPasswordH2);
     submitPassword.reset();
 })
 
-textField.addEventListener('input', (e)=>{
+textField.addEventListener('input', () => {
     let password = textField.value;
-    commonPasswordResult.textContent = ''
-    compromisedPasswordResult.textContent = ''
-    passwordStrengthChecker(password);
+    commonPasswordH2.textContent = ''
+    compromisedPasswordH2.textContent = ''
+    if(compromisedPasswordH2.hasChildNodes()){
+        commonPasswordH2.removeChild(commonPasswordH2.childNodes[0]);
+    }
+    passwordStrength(password);
 
 })
 
-function passwordStrengthChecker(password){
-    for(let key in regexTest){
-        let checkbox =  document.querySelector('input#'+key);
-        console.log(checkbox);
+compromisedPasswordH2.addEventListener('click', ()=>{
+    const h3 = document.createElement('h3');
+    h3.textContent = 'someNumber';
+    compromisedPasswordH2.appendChild(h3);
+})
+
+function passwordStrength(password) {
+    for (let key in regexTest) {
+        let checkbox = document.querySelector('input#' + key);
         let charTest = regexTest[key];
-        switch(charTest.test(password)){
+        switch (charTest.test(password)) {
             case true:
                 checkbox.checked = true;
                 checkbox.disabled = false;
@@ -45,25 +53,32 @@ function passwordStrengthChecker(password){
     }
 }
 
-function isPasswordCompromised(password){
-    const hashedPassword =  sha1(password).toUpperCase();
-    const hashedPrefix =  hashedPassword.substring(0,5);
-    const hashedSuffix = hashedPassword.substring(5,40);
+function isPasswordCompromised(password) {
+    const hashedPassword = sha1(password).toUpperCase();
+    const hashedPrefix = hashedPassword.substring(0, 5);
+    const hashedSuffix = hashedPassword.substring(5, 40);
     fetch(`https://api.pwnedpasswords.com/range/${hashedPrefix}`)
-    .then(Response => Response.text())
-    .then(text => {
-        const resultHashes =text.split('\r\n').map(hash => hash.substring(0,35));
-        passwordComparison(resultHashes,hashedSuffix,compromisedPasswordH2);
-    })
-    .catch(err => console.error(err))
-
+        .then(Response => Response.text())
+        .then(text => {
+            const resultHashes = text.split('\r\n').map(hash => hash.substring(0, 35));
+            passwordComparison(resultHashes, hashedSuffix, compromisedPasswordH2);
+        })
+        .catch(err => console.error(err))
 }
 
-function passwordComparison(arr,str,node){
+function passwordComparison(arr, str, node) {
     const matchedString = arr.find(ele => ele === str);
-    if(matchedString){
-        node.textContent = 'Yes';
-    }else{
+    if (matchedString) {
+        if (node === compromisedPasswordH2) {
+            node.textContent = 'Yes, Click To See How Many Times It Has Been Breached'
+        } else {
+            node.textContent = 'Yes';
+        }
+    } else {
         node.textContent = 'No';
     }
+}
+
+function numberOfTimesBreached(password){
+
 }
